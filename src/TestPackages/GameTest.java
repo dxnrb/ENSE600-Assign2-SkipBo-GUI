f6c276d1642f7520720a9a87f4f04eb84bb9b82d
danie <danie@Zeus>
2025-09-03 20:53:38 +1200

    Initialised

---
+++ src/TestPackages/GameTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TestPackages;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author danie
+ */
+public class GameTest {
+    
+    @Test
+    public void testAddScore() {
+        int result = 10 + 5;
+        assertEquals(15, result, "Score should add correctly");
+    }
+}
